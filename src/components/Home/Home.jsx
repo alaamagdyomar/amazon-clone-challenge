421d42b3196fd49ead690021700394fc97c61919
alaa magdy <alaa_magdy@outlook.com>
2021-08-04 18:14:34 +0200

    finish product component logic

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,16 +12,53 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          {/* product */}
-          <Product />
-          {/* product */}
+          <Product
+            id="1234"
+            title="The lean startup: How constant innovation creates radically Successful Businesses Paperback"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg "
+            rating={3}
+          />
+          <Product
+            id="12346"
+            title="Kenwood KMix Stand Mixer for Baking , Stylish Kitchen Mixer with k-beater, Dough Hook and Whisk , 5 Kitre Glass Bowl"
+            price={40.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          {/* product */}
-          {/* product */}
-          {/* product */}
+          <Product
+            id="123465"
+            title="Samsung LC49RG90SSUXEN 49 Curved Led Gaming Monitor"
+            price={888.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg "
+            rating={5}
+          />
+          <Product
+            id="1234656"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa , Charcoal Fabric"
+            price={70.99}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            rating={5}
+          />
+          <Product
+            id="1234657"
+            title="New Apple iPad Pro (12.9-inch,Wi-fi,128GB) - Silver (4th Generation)"
+            price={2598}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg "
+            rating={5}
+          />
         </div>
-        <div className="home__row">{/* product */}</div>
+        <div className="home__row">
+          <Product
+            id="13465"
+            title="Samsung LC49RG()SSUXEN 49 Curved Led Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={2000}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg "
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup How constant innovation creates radically</p>
+        <p className="product__title">{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
+          {Array(rating) // logic to take the lenth of the array and return back the star as rating
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg "
-        alt=""
-      />
+      <img src={image} alt="" />
       <button>Add To Basket</button>
     </div>
   );
