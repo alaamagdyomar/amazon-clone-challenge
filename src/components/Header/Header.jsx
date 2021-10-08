ace9436f0fc301ddecb6619c619503142f7f318f
alaa magdy <alaa_magdy@outlook.com>
2021-10-09 00:19:00 +0200

    header counter for the basket items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import { ShoppingBasket } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import { useStateValue } from "../../StateProvider";
 
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="header">
       <Link to="/">
@@ -34,7 +36,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasket />
-            <span className="header__optionLineTow header__basketCount">0</span>
+            <span className="header__optionLineTow header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
