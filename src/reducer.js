d5dc0e138fb1118ad7eb2b266b3870796cc67a84
alaa magdy <alaa_magdy@outlook.com>
2021-10-10 17:21:25 +0200

    finish subtotal component with functionality

--- src/reducer.js
+++ src/reducer.js
@@ -14,6 +14,9 @@ export const initialState = {
   user: null,
 };
 
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_BASKET":
